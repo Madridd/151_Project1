52bab779d0371465894e68fa3a5392fff786f67e
Ivan Madrid <ivanmadrid@Ivans-MBP.zyxel.com>
2020-09-02 14:51:48 -0500

    mutipleBubbletrails

--- sketch.js
+++ sketch.js
@@ -1,53 +1,72 @@
-let x = 450;
-let y = 450;
-let xspeed = 4;
-let yspeed = 4;
-let r;
-let g;
-let b;
-
+let circ = []; // array of bubble objects
+let r,g,b;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(225,100,180);
   r = random(255);
   g = random(255);
   b= random(255);
-
-}//end ofsetup
+  // Create objects
+  for (let i = 0; i < 10; i++) {
+    circ.push(new Bubble());
+  }
+}
 
 function draw() {
-  ellipse(x,y,100,100);
-  fill(r,g,b);
-  stroke(22);
+  background(50, 89, 100);
 
-  if(x > width || x < 50){
-  xspeed = -xspeed
-  }
-  if(y > height || y < 50){
-  yspeed = -yspeed
-  }
+  for (let i = 0; i < circ.length; i++) {
+    //for(let j = 0; j< 50; j++){
+      circ[i].move();
+      circ[i].display();
+    //}
 
-  x = x + xspeed;
-  y = y + yspeed;
+  }
 
   if(mouseIsPressed){
-    //fill(255,100,180,20);
-    //^will use a gradient to change between colors^
-    //if taken out will allow mutiple color design
-
-    //ellipse(mouseX,mouseY,100,100);
-    //^will allow to drag the circle^
-
     r = random(255);
     g = random(255);
     b = random(255);
-  }
+    circ.push(new Bubble());
 
+
+  }
 }
 
+// bubble class
+class Bubble {
+  constructor() {
+    this.x = random(width);
+    this.y = random(height);
+    this.diameter = random(50, 80);
+    this.xspeed = 1;
+    this.yspeed = 2;
+    this.history = [];
+  }
 
-function keyPressed() {
-  if (key === 's') {
-    saveCanvas('image1s', 'png');
+  move() {
+    if(this.x > width || this.x <0){this.xspeed = -this.xspeed}
+    if(this.y > height|| this.y <0){this.yspeed = -this.yspeed}
+    this.x = this.x + this.xspeed;
+    this.y = this.y+ this.yspeed;
+
+    //
+    var v = createVector(this.x, this.y);
+    this.history.push(v);
+
+    if(this.history.length > 50){
+      this.history.splice(0,2);
+    }
+
+  }
+
+  display() {
+    ellipse(this.x, this.y, this.diameter, this.diameter);
+    fill(r,g,b);
+
+    for(var i = 0; i < this.history.length; i++){
+      var pos = this.history[i];
+      ellipse(pos.x, pos.y, this.diameter, this.diameter);
+      fill(r,g,b);
+    }
   }
 }
